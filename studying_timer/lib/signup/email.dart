c6e22ad930568871637cafa64a81d2be091f5129
조용제 <whehdrms6810@naver.com>
2022-08-23 14:20:46 +0900

    [CREATE] email.dart

---
+++ studying_timer/lib/signup/email.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:studying_timer/common/common.dart';
+import 'package:studying_timer/signup/make_name.dart';
+
+class Email extends StatefulWidget {
+  const Email({Key? key}) : super(key: key);
+
+  @override
+  State<Email> createState() => _EmailState();
+}
+
+class _EmailState extends State<Email> {
+  @override
+  Widget build(BuildContext context) {
+    final _pwController = TextEditingController();
+    String pw = '';
+
+    final _emailController = TextEditingController();
+    String email = '';
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.black,
+                        size: 25.h,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "열정 품은 타이머",
+                            style: TextStyle(
+                                fontSize: 25.sp,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "회원가입",
+                            style: TextStyle(
+                                fontSize: 25.sp,
+                                color: CommonColor.orange,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ]),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 60.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 28.w),
+                child: Text(
+                  "계정",
+                  style: TextStyle(fontSize: 15.sp, color: CommonColor.orange),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 28.w, right: 28.w, top: 10.h),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: '이메일 입력',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                    ),
+                    focusedErrorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 28.w, right: 28.w),
+                child: TextField(
+                  controller: _pwController,
+                  decoration: const InputDecoration(
+                    labelText: '비밀번호 입력',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                    ),
+                    focusedErrorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 28.w, right: 28.w),
+                child: TextField(
+                  controller: _pwController,
+                  decoration: const InputDecoration(
+                    labelText: '비밀번호 확인',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                    ),
+                    focusedErrorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 30.h, left: 28.w, right: 28.w),
+                child: ElevatedButton(
+                  onPressed: () {
+                    
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Make_Name()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: CommonColor.orange,
+                    minimumSize: Size(double.infinity, 40.h),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.h)),
+                  ),
+                  child: Text(
+                    "회원가입",
+                    style: TextStyle(
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
