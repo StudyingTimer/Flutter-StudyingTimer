58834c4a544c07cfcbfff2eee47a72bbabf9dfa6
조용제 <whehdrms6810@naver.com>
2022-08-25 16:02:30 +0900

    [UPDATE] path

--- studying_timer/lib/main.dart
+++ studying_timer/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:studying_timer/common/common.dart';
-import 'package:studying_timer/signup/login.dart';
-import 'package:studying_timer/signup/start_signup.dart';
+import 'package:studying_timer/screens/signup/login.dart';
+import 'package:studying_timer/screens/signup/start_signup.dart';
 
 void main() {
   runApp(const MyApp());
