58687fce15885ab6e642984c31731c69ef68d214
조용제 <whehdrms6810@naver.com>
2022-09-01 20:22:35 +0900

    [CREATE] ranking.dart

---
+++ studying_timer/lib/screens/bottombar/ranking.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Rank extends StatelessWidget {
+  const Rank({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: const[
+            Text("Rank", style: TextStyle(fontSize: 30),)
+          ],
+        ),
+      ),
+    );
+  }
+}
