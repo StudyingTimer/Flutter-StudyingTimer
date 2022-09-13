3eedd8a20c2c438b12ac604d34741b5e87b17855
조용제 <whehdrms6810@naver.com>
2022-09-13 21:12:23 +0900

    [CREATE] bottom.dart

---
+++ studying_timer/lib/screens/bottombar/bottom.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:studying_timer/screens/bottombar/home.dart';
+import 'package:studying_timer/screens/bottombar/ranking.dart';
+import 'package:studying_timer/screens/bottombar/status.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:studying_timer/screens/bottombar/todo.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  var _currentIndex = 0;
+  final List<Widget> _children = [
+    const Home(),
+    const Rank(),
+    const Status(),
+    const Todo()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: SalomonBottomBar(
+          currentIndex: _currentIndex,
+          onTap: (i) => setState(() => _currentIndex = i),
+          items: [
+            /// Home
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.home),
+              title: const Text("Home"),
+              selectedColor: Colors.purple,
+            ),
+
+            /// Likes
+            SalomonBottomBarItem(
+              icon: const Icon(FontAwesomeIcons.clipboardList),
+              title: const Text("Todo"),
+              selectedColor: Colors.teal,
+            ),
+
+            /// Search
+            SalomonBottomBarItem(
+              icon: const Icon(FontAwesomeIcons.rankingStar),
+              title: const Text("Rank"),
+              selectedColor: Colors.pink,
+            ),
+
+            /// Profile
+            SalomonBottomBarItem(
+              icon: const Icon(FontAwesomeIcons.chartBar),
+              title: const Text("Status"),
+              selectedColor: Colors.orange,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
