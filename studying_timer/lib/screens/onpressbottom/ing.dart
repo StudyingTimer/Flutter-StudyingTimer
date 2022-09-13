d442ebc17e49dd30ebdd7c2856b8d082ebfec075
조용제 <whehdrms6810@naver.com>
2022-09-13 21:12:35 +0900

    [CREATE] ing.dart

---
+++ studying_timer/lib/screens/onpressbottom/ing.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:studying_timer/model/timer.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:studying_timer/screens/bottombar/bottom.dart';
+import 'package:studying_timer/screens/bottombar/home.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Ing extends StatefulWidget {
+  final int hour, minute, second, i;
+  final List<TimerModel> timerList;
+
+  const Ing(
+      {Key? key,
+      required this.hour,
+      required this.minute,
+      required this.second,
+      required this.i,
+      required this.timerList})
+      : super(key: key);
+
+  @override
+  State<Ing> createState() => _IngState();
+}
+
+class _IngState extends State<Ing> {
+  late Timer timer = timer;
+  NumberFormat formatter = NumberFormat("00");
+  @override
+  void dispose() {
+    timer.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    start();
+  }
+
+  void start() {
+    timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      setState(() {
+        widget.timerList[widget.i].second++;
+        if (widget.timerList[widget.i].second == 60) {
+          widget.timerList[widget.i].second = 0;
+          widget.timerList[widget.i].minute++;
+          if (widget.timerList[widget.i].minute == 60) {
+            widget.timerList[widget.i].minute = 0;
+            widget.timerList[widget.i].hour++;
+          }
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 320.h, top: 320.h),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 10.w),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.pause_circle,
+                        color: Colors.white,
+                      ),
+                      iconSize: 50.r,
+                    ),
+                  ),
+                  Text(
+                    formatter.format(widget.timerList[widget.i].hour),
+                    style: TextStyle(color: Colors.white, fontSize: 40.sp),
+                  ),
+                  Text(
+                    ":",
+                    style: TextStyle(color: Colors.white, fontSize: 40.sp),
+                  ),
+                  Text(
+                    formatter.format(widget.timerList[widget.i].minute),
+                    style: TextStyle(color: Colors.white, fontSize: 40.sp),
+                  ),
+                  Text(
+                    ":",
+                    style: TextStyle(color: Colors.white, fontSize: 40.sp),
+                  ),
+                  Text(
+                    formatter.format(widget.timerList[widget.i].second),
+                    style: TextStyle(color: Colors.white, fontSize: 40.sp),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "이 기능을 사용하는 동안",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              "이 앱을 제외한 모든앱을 사용할 수 없습니다",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
