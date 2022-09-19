cc115030dc45e291ac6a73e3f336c9a496ad2483
조용제 <whehdrms6810@naver.com>
2022-09-19 21:25:05 +0900

    [ADD] datetime

--- studying_timer/lib/screens/bottombar/home.dart
+++ studying_timer/lib/screens/bottombar/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
 import 'package:flutter/material.dart';
 import 'package:studying_timer/common/common.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -24,11 +26,16 @@ class _HomeState extends State<Home> {
   int second = 00;
   int position1 = 120;
   int position2 = 60;
+  var now = DateTime.now();
+  late String formatDate1 = DateFormat('MM. dd. ').format(now);
+  late String formatData2 = DateFormat('E', 'ko').format(now);
 
   @override
   void dispose() {
     // timer.cancel();
+
     super.dispose();
+    print('home.dart가 끝났습니다');
   }
 
   @override
@@ -37,12 +44,14 @@ class _HomeState extends State<Home> {
     timerList.add(TimerModel("국어"));
     timerList.add(TimerModel("수학"));
     timerList.add(TimerModel("영어"));
+    print('home.dart가 실행되었습니다');
   }
 
   @override
   Widget build(BuildContext context) {
     var emphasis = Provider.of<Emphaisis>(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: CommonColor.orange,
       appBar: AppBar(
         centerTitle: true,
@@ -52,7 +61,8 @@ class _HomeState extends State<Home> {
                 BorderSide(color: Colors.white.withOpacity(0.5), width: 0.5.h)),
         backgroundColor: CommonColor.orange,
         title: Text(
-          "9. 08. (목)",
+          // "9. 08. (목)",
+          "${formatDate1}(${formatData2})",
           style: TextStyle(fontSize: 20.sp),
         ),
         leading: IconButton(
@@ -141,65 +151,65 @@ class _HomeState extends State<Home> {
 
                       //휴식 알림 -> padding.top = 120
                       //타이머 추가 -> padding.top = 60
-                      Stack(
-                        children: [
-                          Padding(
-                            padding:
-                                EdgeInsets.only(top: position1.h, left: 270.w),
-                            child: AnimatedScale(
-                              duration: const Duration(microseconds: 60000),
-                              scale: emphasis.ispressed ? 1 : 0,
-                              child: ElevatedButton(
-                                  onPressed: () {},
-                                  style: ElevatedButton.styleFrom(
-                                      primary: Colors.white,
-                                      minimumSize: Size(55.w, 50.h)),
-                                  child: const Icon(
-                                    Icons.access_alarm_outlined,
-                                    color: Colors.black,
-                                  )),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                EdgeInsets.only(top: position2.h, left: 270.w),
-                            child: AnimatedScale(
-                              duration: const Duration(microseconds: 110000),
-                              scale: emphasis.ispressed ? 1 : 0,
-                              child: ElevatedButton(
-                                  onPressed: () {},
-                                  style: ElevatedButton.styleFrom(
-                                      primary: CommonColor.orange,
-                                      minimumSize: Size(55.w, 50.h)),
-                                  child: Icon(
-                                    FontAwesomeIcons.bookOpen,
-                                    color: Colors.white,
-                                    size: 20.h,
-                                  )),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 180.h, left: 270.w),
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    emphasis.change();
-                                  });
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  shape: const CircleBorder(),
-                                  elevation: 6.0,
-                                  minimumSize: Size(55.w, 55.h),
-                                  primary: Colors.white,
-                                ),
-                                child: Icon(
-                                  emphasis.ispressed ? Icons.close : Icons.add,
-                                  color: CommonColor.orange,
-                                  size: 25.h,
-                                )),
-                          ),
-                        ],
-                      ),
+                      // Stack(
+                      //   children: [
+                      //     Padding(
+                      //       padding:
+                      //           EdgeInsets.only(top: position1.h, left: 270.w),
+                      //       child: AnimatedScale(
+                      //         duration: const Duration(microseconds: 60000),
+                      //         scale: emphasis.ispressed ? 1 : 0,
+                      //         child: ElevatedButton(
+                      //             onPressed: () {},
+                      //             style: ElevatedButton.styleFrom(
+                      //                 primary: Colors.white,
+                      //                 minimumSize: Size(55.w, 50.h)),
+                      //             child: const Icon(
+                      //               Icons.access_alarm_outlined,
+                      //               color: Colors.black,
+                      //             )),
+                      //       ),
+                      //     ),
+                      //     Padding(
+                      //       padding:
+                      //           EdgeInsets.only(top: position2.h, left: 270.w),
+                      //       child: AnimatedScale(
+                      //         duration: const Duration(microseconds: 110000),
+                      //         scale: emphasis.ispressed ? 1 : 0,
+                      //         child: ElevatedButton(
+                      //             onPressed: () {},
+                      //             style: ElevatedButton.styleFrom(
+                      //                 primary: CommonColor.orange,
+                      //                 minimumSize: Size(55.w, 50.h)),
+                      //             child: Icon(
+                      //               FontAwesomeIcons.bookOpen,
+                      //               color: Colors.white,
+                      //               size: 20.h,
+                      //             )),
+                      //       ),
+                      //     ),
+                      //     Padding(
+                      //       padding: EdgeInsets.only(top: 180.h, left: 270.w),
+                      //       child: ElevatedButton(
+                      //           onPressed: () {
+                      //             setState(() {
+                      //               emphasis.change();
+                      //             });
+                      //           },
+                      //           style: ElevatedButton.styleFrom(
+                      //             shape: const CircleBorder(),
+                      //             elevation: 6.0,
+                      //             minimumSize: Size(55.w, 55.h),
+                      //             primary: Colors.white,
+                      //           ),
+                      //           child: Icon(
+                      //             emphasis.ispressed ? Icons.close : Icons.add,
+                      //             color: CommonColor.orange,
+                      //             size: 25.h,
+                      //           )),
+                      //     ),
+                      //   ],
+                      // ),
                     ])))
           ],
         ),
