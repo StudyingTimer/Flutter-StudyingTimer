e2b1fae080ac2b8a73ac0ad7e3c9e0a1dba9e0cd
조용제 <whehdrms6810@naver.com>
2022-10-17 21:11:52 +0900

    [DELETE} import

--- studying_timer/lib/provider/subjectlist.dart
+++ studying_timer/lib/provider/subjectlist.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:studying_timer/model/timer.dart';
-import 'package:studying_timer/widget/timerList.dart';
 
 class SubjectList extends ChangeNotifier {
   // ChangeNotifier를 extends한다.
