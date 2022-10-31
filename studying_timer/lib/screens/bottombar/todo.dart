c772b501f181112f20693310019bcb99517342f2
조용제 <whehdrms6810@naver.com>
2022-10-31 21:29:11 +0900

    [ADD] animation

--- studying_timer/lib/screens/bottombar/todo.dart
+++ studying_timer/lib/screens/bottombar/todo.dart
@@ -12,88 +12,107 @@ class Todo extends StatefulWidget {
 
 class _TodoState extends State<Todo> {
   var now = DateTime.now();
+  bool ispressed = false;
   late String formatDate1 = DateFormat('MM. dd. ').format(now);
   late String formatData2 = DateFormat('E', 'ko').format(now);
+  int buttonleft = 90;
+  int buttonright = 90;
+  int buttontop = 380;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0.0,
-        shape: Border(
-            bottom:
-                BorderSide(color: Colors.white.withOpacity(0.5), width: 0.5.h)),
-        backgroundColor: Colors.transparent,
-        title: Text(
-          // "9. 08. (목)",
-          "${formatDate1}(${formatData2})",
-          style: TextStyle(fontSize: 25.sp, color: Colors.black),
-        ),
-        actions: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 14.w),
-                child: Text(
-                  "To-do",
-                  style: TextStyle(
-                      fontSize: 15.sp,
-                      color: Colors.black.withOpacity(0.5),
-                      fontWeight: FontWeight.w500),
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        appBar: null,
+        // AppBar(
+        //   centerTitle: true,
+        //   elevation: 0.0,
+        //   shape: Border(
+        //       bottom:
+        //           BorderSide(color: Colors.white.withOpacity(0.5), width: 0.5.h)),
+        //   backgroundColor: Colors.transparent,
+        //   title: Text(
+        //     // "9. 08. (목)",
+        //     "${formatDate1}(${formatData2})",
+        //     style: TextStyle(fontSize: 25.sp, color: Colors.black),
+        //   ),
+        //   actions: [
+        //     Column(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         Padding(
+        //           padding: EdgeInsets.only(right: 14.w),
+        //           child: Text(
+        //             "To-do",
+        //             style: TextStyle(
+        //                 fontSize: 15.sp,
+        //                 color: Colors.black.withOpacity(0.5),
+        //                 fontWeight: FontWeight.w500),
+        //           ),
+        //         ),
+        //       ],
+        //     )
+        //   ],
+        // ),
+        body: Stack(
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  ispressed = !ispressed;
+                });
+              },
+              child: AnimatedPadding(
+                padding: EdgeInsets.only(
+                    left: buttonleft.w, right: buttonright.w, top: buttontop.h),
+                duration: const Duration(microseconds: 100000),
+                child: Container(
+                  width: double.infinity,
+                  height: 55.h,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.r),
+                      color: Colors.black),
                 ),
               ),
-            ],
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 1.h,
-                color: Colors.black.withOpacity(0.3),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  ispressed = !ispressed;
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.only(left: 154.w, top: 397.h),
+                child: AnimatedOpacity(
+                    opacity: ispressed ? 0 : 1,
+                    duration: const Duration(microseconds: 100000),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 15.h,
+                    )),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 40.h),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Material(
-                      elevation: 2,
-                      child: Container(
-                        width: 140.w,
-                        height: 180.h,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10.r),
-                                topRight: Radius.circular(10.r))),
-                      ),
-                    ),
-                    Material(
-                      elevation: 2,
-                      child: Container(
-                        width: 140.w,
-                        height: 180.h,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10.r),
-                                topRight: Radius.circular(10.r))),
-                      ),
-                    ),
-                  ],
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  ispressed = !ispressed;
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.only(left: 154.w, top: 397.h),
+                child: AnimatedOpacity(
+                  opacity: ispressed ? 0 : 1,
+                  duration: const Duration(microseconds: 100000),
+                  child: Text(
+                    "Add item",
+                    style: TextStyle(fontSize: 13.5.sp, color: Colors.white),
+                  ),
                 ),
-                
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+            )
+          ],
+        ));
   }
 }
