10d39acfa237c3647448a95ccf5898a7f3084b90
조용제 <whehdrms6810@naver.com>
2022-11-01 21:35:49 +0900

    [ADD] accessToken, bsmispressed, inputaccesstoken

--- studying_timer/lib/provider/emphasis.dart
+++ studying_timer/lib/provider/emphasis.dart
@@ -3,11 +3,28 @@ import 'package:flutter/material.dart';
 class Emphaisis extends ChangeNotifier {
   // ChangeNotifier를 extends한다.
   bool _ispressed = false;
+  String _accessToken = "";
+  bool _bsmispressed = false;
 
   bool get ispressed => _ispressed;
-
+  bool get bsmispressed => _bsmispressed;
+  String get accessToken => _accessToken;
   void change() {
     _ispressed = !ispressed;
+    notifyListeners();
+  }
+
+  void bsmchange() {
+    _bsmispressed = true;
+    notifyListeners();
+  }
+
+  void inputaccesstoken(String access) {
+    print("access = $access");
+
+    _accessToken = access;
+    // ignore: avoid_print
+    print("accessToken = $accessToken");
     notifyListeners();
   }
 }
