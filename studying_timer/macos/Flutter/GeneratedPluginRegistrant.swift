af29f1f27ccf2b44de31827461ae1eee0c85829b
조용제 <whehdrms6810@naver.com>
2022-11-01 21:36:12 +0900

    [ADD] webauth

--- studying_timer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ studying_timer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_web_auth
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
