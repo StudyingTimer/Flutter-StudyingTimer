9db3a9911886e1a72499f30f8e6faa844ff04088
조용제 <whehdrms6810@naver.com>
2022-11-03 08:13:44 +0900

    [ADD] webview

--- studying_timer/lib/screens/signup/bsm_webview.dart
+++ studying_timer/lib/screens/signup/bsm_webview.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:studying_timer/common/common.dart';
 import 'package:studying_timer/provider/emphasis.dart';
+import 'package:studying_timer/screens/signup/make_name.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
@@ -32,8 +33,6 @@ class _WebviewState extends State<Webview> {
       WebView.platform = SurfaceAndroidWebView();
     }
   }
-  
-  
 
   // _postRequest(var pressed) async {
   //   var parsingData;
@@ -50,7 +49,7 @@ class _WebviewState extends State<Webview> {
   //       var parsingData = jsonDecode(utf8.decode(response.bodyBytes));
   //       print("parsingData = $parsingData");
   //       accessToken = parsingData['accessToken']['value'];
-        
+
   //       // ignore: avoid_print
   //       print("22222222222222222221222222222222222222222222222222222222");
   //       // ignore: avoid_print
@@ -62,9 +61,6 @@ class _WebviewState extends State<Webview> {
   //           context, MaterialPageRoute(builder: (_) => const BsmSuccess()));
   //       // return parsingData;
   //     }
-    
-
-    
 
   @override
   Widget build(BuildContext context) {
@@ -74,17 +70,19 @@ class _WebviewState extends State<Webview> {
           ? Center(child: SpinKitWave(color: CommonColor.orange))
           : WebView(
               initialUrl:
-                  'https://auth.bssm.kro.kr/oauth?clientId=4c81669f&redirectURI=https://bgit.bssm.kro.kr/oauth/bsm',
+                  'https://auth.bssm.kro.kr/oauth?clientId=5f034939&redirectURI=http://localhost:3000/oauth',
               javascriptMode: JavascriptMode.unrestricted,
               navigationDelegate: (NavigationRequest request) {
                 if (request.url.contains("?code=")) {
                   setState(() {
                     code = request.url.split("?code=")[1];
                     close = true;
-                  });x
+                    print(code);
+                  });
                   // _postRequest(emphaisis);
                   emphaisis.bsmchange();
-
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const Make_Name()));
                   // do not navigate
                   return NavigationDecision.prevent;
                 } else if (request.url.contains(url)) {}
@@ -98,4 +96,3 @@ class _WebviewState extends State<Webview> {
     );
   }
 }
-
