8b0b9e20bb17fac7bfd14585d57554e9f4bc7db7
조용제 <whehdrms6810@naver.com>
2022-11-03 21:49:19 +0900

    [ADD] animation

--- studying_timer/lib/screens/bottombar/todo.dart
+++ studying_timer/lib/screens/bottombar/todo.dart
@@ -13,18 +13,31 @@ class Todo extends StatefulWidget {
 class _TodoState extends State<Todo> {
   var now = DateTime.now();
   bool ispressed = false;
+  bool addispressed = false;
   final myController = TextEditingController();
   late String formatDate1 = DateFormat('MM. dd. ').format(now);
   late String formatData2 = DateFormat('E', 'ko').format(now);
   int buttonleft = 90;
   int buttonright = 90;
   int buttontop = 380;
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
+        myController.clear();
+        setState(() {
+          ispressed = false;
+          addispressed = false;
+        });
       },
       child: Scaffold(
           resizeToAvoidBottomInset: false,
@@ -63,77 +76,59 @@ class _TodoState extends State<Todo> {
           body: Stack(
             children: [
               Padding(
-                padding: EdgeInsets.only(left: 150.w, top: 397.h),
-                child: TextField(
-                  controller: myController,
-                  autofocus: ispressed ? true : false,
-                  style: TextStyle(fontSize: 15.sp, color: Colors.black),
-                  textAlign: TextAlign.start,
-                  cursorColor: Colors.black.withOpacity(0.4),
-                  textAlignVertical: TextAlignVertical.center,
-                  maxLength: 20,
-                  decoration: InputDecoration(
-                    counterText: "",
-                    hintStyle: TextStyle(
-                      color: Colors.black.withOpacity(0.5),
-                      fontSize: 19.sp,
-                    ),
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    contentPadding: const EdgeInsets.only(
-                        left: 15, bottom: 11, top: 11, right: 15),
-                  ),
-                ),
-              ),
+                  padding: EdgeInsets.only(left: 155.w, top: 100.h),
+                  child: Text(
+                    "todo",
+                    style: TextStyle(fontSize: 18.sp, color: Colors.black),
+                  )),
               GestureDetector(
                 onTap: () {
                   setState(() {
-                    ispressed = !ispressed;
+                    ispressed = true;
                   });
                 },
                 child: AnimatedPadding(
                   padding: EdgeInsets.only(
-                      left: ispressed ? 23.w : 90.w,
-                      right: ispressed ? 23.w : 90.w,
-                      top: buttontop.h),
+                      left: ispressed
+                          ? addispressed
+                              ? 156.5.w
+                              : 23.w
+                          : 90.w,
+                      right: ispressed
+                          ? addispressed
+                              ? 156.5.w
+                              : 23.w
+                          : 90.w,
+                      top: addispressed ? 650.h : 380.h),
                   duration: const Duration(microseconds: 240000),
                   child: AnimatedContainer(
                     duration: const Duration(microseconds: 240000),
                     width: double.infinity,
                     height: 60.h,
                     decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.7),
+                            spreadRadius: 2,
+                            blurRadius: 7.0,
+                            offset:
+                                Offset(0.w, 3.h), // changes position of shadow
+                          ),
+                        ],
                         border: Border.all(width: 0.5.w, color: Colors.black),
                         borderRadius: BorderRadius.circular(30.r),
-                        color: ispressed ? Colors.white : Colors.black),
+                        color: ispressed
+                            ? addispressed
+                                ? Colors.black
+                                : Colors.white
+                            : Colors.black),
                   ),
                 ),
               ),
               GestureDetector(
                 onTap: () {
                   setState(() {
-                    ispressed = !ispressed;
-                  });
-                },
-                child: AnimatedPadding(
-                    duration: const Duration(microseconds: 160000),
-                    padding: EdgeInsets.only(
-                        left: ispressed ? 300.w : 125.w, top: 398.h),
-                    child: AnimatedContainer(
-                      duration: const Duration(microseconds: 150000),
-                      child: Icon(
-                        Icons.add,
-                        color: ispressed ? Colors.black : Colors.white,
-                        size: 23.h,
-                      ),
-                    )),
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    ispressed = !ispressed;
+                    ispressed = true;
                   });
                 },
                 child: Padding(
@@ -147,7 +142,78 @@ class _TodoState extends State<Todo> {
                     ),
                   ),
                 ),
-              )
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    ispressed = true;
+                  });
+                },
+                child: AnimatedPadding(
+                    duration: const Duration(microseconds: 180000),
+                    padding: EdgeInsets.only(
+                        left: 50.w, top: addispressed ? 200.h : 378.h),
+                    child: ispressed
+                        ? TextField(
+                            focusNode: myFocusNode,
+                            controller: myController,
+                            autofocus: ispressed ? true : false,
+                            style:
+                                TextStyle(fontSize: 15.sp, color: Colors.black),
+                            textAlign: TextAlign.start,
+                            cursorColor: Colors.black.withOpacity(0.4),
+                            textAlignVertical: TextAlignVertical.center,
+                            maxLength: 15,
+                            decoration: InputDecoration(
+                              counterText: "",
+                              hintStyle: TextStyle(
+                                color: Colors.black.withOpacity(0.5),
+                                fontSize: 19.sp,
+                              ),
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 15, bottom: 11, top: 11, right: 15),
+                            ),
+                          )
+                        : const Text("")),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    ispressed
+                        ? {
+                            addispressed = true,
+                            FocusScope.of(context).unfocus()
+                          }
+                        : {};
+                  });
+                },
+                child: AnimatedPadding(
+                    duration: const Duration(microseconds: 160000),
+                    padding: EdgeInsets.only(
+                        left: ispressed
+                            ? addispressed
+                                ? 171.w
+                                : 300.w
+                            : 125.w,
+                        top: addispressed ? 668.h : 398.h),
+                    child: AnimatedContainer(
+                      duration: const Duration(microseconds: 150000),
+                      child: Icon(
+                        Icons.add,
+                        color: ispressed
+                            ? addispressed
+                                ? Colors.white
+                                : Colors.black
+                            : Colors.white,
+                        size: 23.h,
+                      ),
+                    )),
+              ),
             ],
           )),
     );
