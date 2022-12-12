dd13f972a3f8bf36c921056ea00d6ec0d1f1771e
조용제 <whehdrms6810@naver.com>
2022-12-12 21:49:03 +0900

    [ADD] toastmessage, http response

--- studying_timer/lib/screens/signup/bsm_webview.dart
+++ studying_timer/lib/screens/signup/bsm_webview.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:studying_timer/common/common.dart';
 import 'package:studying_timer/provider/emphasis.dart';
 import 'package:studying_timer/screens/bottombar/bottom.dart';
+import 'package:studying_timer/screens/loading.dart';
 import 'package:studying_timer/screens/signup/make_name.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -59,15 +60,30 @@ class _WebviewState extends State<Webview> {
         fontSize: 16.sp);
   }
 
+  void toastmessage2() {
+    Fluttertoast.showToast(
+        msg: "회원가입 창으로 이동합니다",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey,
+        textColor: Colors.white,
+        fontSize: 16.sp);
+  }
+
   _postRequest(var emphaisis) async {
     var parsingData;
+
     print("code");
+    print(code);
     print("postrequest 실행");
     String url =
-        'http://study-timer-aws.ap-northeast-2.elasticbeanstalk.com/student/oauth/$code';
-    http.Response response = await http.post(Uri.parse(url));
+        'http://java-project-studytimer.ap-northeast-2.elasticbeanstalk.com/student/oauth/$code';
+    http.Response response = await http.get(Uri.parse(url));
     int statuscode = response.statusCode;
+
     print(statuscode);
+
     if (statuscode == 200) {
       var parsingData = jsonDecode(utf8.decode(response.bodyBytes));
       print("parsingData = $parsingData");
@@ -79,10 +95,15 @@ class _WebviewState extends State<Webview> {
       print(accessToken);
       emphaisis.inputaccesstoken(accessToken);
       if (parsingData['student']) {
+        toastmessage();
+
         // ignore: use_build_context_synchronously
         Navigator.push(
-            context, MaterialPageRoute(builder: (_) => const MyPage()));
+            context,
+            MaterialPageRoute(
+                builder: (_) => Loading(accessToken: accessToken)));
       } else {
+        toastmessage1();
         // ignore: use_build_context_synchronously
         Navigator.push(
             context, MaterialPageRoute(builder: (_) => const Make_Name()));
@@ -92,7 +113,7 @@ class _WebviewState extends State<Webview> {
     } else {
       // ignore: use_build_context_synchronously
       Navigator.pop(context);
-      toastmessage1();
+      toastmessage2();
     }
   }
 
@@ -104,16 +125,28 @@ class _WebviewState extends State<Webview> {
           ? Center(child: SpinKitWave(color: CommonColor.orange))
           : WebView(
               initialUrl:
-                  'https://auth.bssm.kro.kr/oauthhttps://auth.bssm.kro.kr/oauth?clientId=5f034939&redirectURI=http://localhost:3000/oauth',
+                  'https://auth.bssm.kro.kr/oauth?clientId=5f034939&redirectURI=http://localhost:3000/oauth',
               javascriptMode: JavascriptMode.unrestricted,
               navigationDelegate: (NavigationRequest request) {
                 if (request.url.contains("?code=")) {
                   setState(() {
                     code = request.url.split("?code=")[1];
+                    print("code : $code");
+                    print("code1 : $code");
+                    print("code2 : $code");
+                    print("code3 : $code");
+                    print("code4 : $code");
+                    print("code5 : $code");
+                    print("code6 : $code");
+                    print("code7 : $code");
+                    print("code8 : $code");
+                    print("code9 : $code");
+                    print("code10 : $code");
+                    print("code11 : $code");
+                    _postRequest(emphaisis);
                     close = true;
-                    print(code);
                   });
-                  _postRequest(emphaisis);
+                  // _postRequest(emphaisis);
                   emphaisis.bsmchange();
                   return NavigationDecision.prevent;
                 } else if (request.url.contains(url)) {}
