8fe768993b78d2d43a73c5d42806c58785658587
조용제 <whehdrms6810@naver.com>
2022-12-15 20:28:00 +0900

    [ADD] profile UI

--- studying_timer/lib/screens/bottombar/status.dart
+++ studying_timer/lib/screens/bottombar/status.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:studying_timer/provider/signup.dart';
+import 'package:studying_timer/screens/signup/bsm_webview.dart';
 
 class Status extends StatelessWidget {
   const Status({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var signupData = Provider.of<SignupData>(context);
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: const[
-            Text("Status", style: TextStyle(fontSize: 30),)
-          ],
-        ),
+        body: Center(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 76.h,
+          ),
+          Icon(
+            Icons.account_circle,
+            color: const Color(0xffD9D9D9),
+            size: 97.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.h, bottom: 130.h),
+            child: Text(
+              "${signupData.name} 님",
+              style: TextStyle(
+                  fontSize: 20.sp,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => const Webview()));
+            },
+            child: const Text("로그아웃"),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
