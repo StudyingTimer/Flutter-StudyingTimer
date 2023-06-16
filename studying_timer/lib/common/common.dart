6362c3da2252ec498041d9c450934e9df0084eed
Lovingcats <whehdrms6810@naver.com>
2023-06-16 09:30:31 +0900

    [ADD] color

--- studying_timer/lib/common/common.dart
+++ studying_timer/lib/common/common.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 class CommonColor {
   static Color orange = const Color(0xFF20BBFE);
+  static Color red = const Color(0xFFE6333F);
+  static Color blue = const Color(0xFF006AB7);
+  static Color yellow = const Color(0xFFF3A941);
+  static Color gray01 = const Color(0xFFF4F4F4);
+  static Color gray02 = const Color(0xFFB2B2B2);
 }
